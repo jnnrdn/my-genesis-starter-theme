593932c028348c69c4b6a38fb87a498c9cbc70a5
Jenny <jenny@webbviken.se>
2016-11-17 17:04:56 +0100

    Part 3a – Automating Tasks with Gulp Developer’s Genesis Starter Child Theme (Know the Code), episode 1-5.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp'),
+    bourbon = require('bourbon').includePaths,
+    neat = require('bourbon-neat').includePaths,
+    sass = require('gulp-sass');
+
+gulp.task('styles', function(){
+
+    return gulp.src('assets/sass/style.scss')
+        .pipe( sass({
+            includePaths: [].concat( bourbon, neat ),
+            errLogToConsole: true,
+            outputStyle: 'expanded' //Options: nested, expanded, compact, compressed
+        }))
+        .pipe(gulp.dest('./'));
+
+});
